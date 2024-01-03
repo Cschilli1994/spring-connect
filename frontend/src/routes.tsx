1bb55112756a68b6fdfc5939c7a1a8bd86a361c9
Chris Schillinger <chris@koptional.com>
2024-01-02 22:13:52 -0300

    removed demo files

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,13 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
-import { SecurityTest } from "./pages/SecurityTest";
 
 export const routes = createBrowserRouter([
   {
     path: "/",
     element: <div>Hello world!</div>,
-  },
-  {
-    path: "/security",
-    element: <SecurityTest />,
   },
 ]);
